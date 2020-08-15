5581601dff9ba1a49c4b5fcd9ee3e2ef330b7fbd
magaFrelancer <ban0le4@gmail.com>
2020-08-15 21:13:17 +0300

    header

---
+++ js/header-select.js
@@ -0,0 +1,20 @@
+const selectAllElements = document.querySelectorAll('[data-select]');
+
+selectAllElements.forEach(item => {
+    item.addEventListener('click', function () {
+        const realSelect = this.nextElementSibling;
+
+        if (event.target.hasAttribute('data-select-item')) {
+            let itemTitle = event.target.getAttribute('data-select-item');
+            this.querySelector('[data-select-title]').textContent = itemTitle;
+            this.querySelector('.header-select__dropdown').classList.toggle('hidden');
+            if(realSelect) {
+                realSelect.value = itemTitle
+            }
+            
+
+        } else {
+            this.querySelector('.header-select__dropdown').classList.toggle('hidden');
+        }
+    })
+})
