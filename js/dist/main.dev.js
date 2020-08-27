0a87ab86fbcaedecc05f0018a93dea0db47974d8
magaFrelancer <ban0le4@gmail.com>
2020-08-27 09:02:08 +0300

    закончил слайдер

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -3,6 +3,8 @@
 $(document).ready(function () {
   $("#carousel-header").owlCarousel({
     items: 1,
-    nav: true
+    nav: true,
+    navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+    loop: true
   });
 });
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $("#carousel-header").owlCarousel({
         items: 1,
-        nav: true
+        nav: true,
+        navText:['<i class="fas fa-arrow-left"></i>','<i class="fas fa-arrow-right"></i>'],
+        loop:true
     });
   });
 
